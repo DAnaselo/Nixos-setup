598b8e34c538981ba06d68c2bd22008b83d9d960
DAnaselo <abukaffanas5@gmail.com>
2024-05-27 17:20:25 +0300

    gnome wayland

--- de/gnome.nix
+++ de/gnome.nix
@@ -6,7 +6,10 @@
      enable = true;
      videoDrivers = [ "nvidia" ];
      desktopManager.gnome.enable = true;
-     displayManager.gdm.enable  = true;
+     displayManager.gdm = {
+       enable = true;
+       wayland = true;
+     };
      desktopManager.xterm.enable = false;
      excludePackages = [ pkgs.xterm ];
    };
@@ -14,5 +17,21 @@
 
  # Gnome Doesn't Like PulseAudio
  hardware.pulseaudio.enable = false;
+
+  # Dynamic triple buffering Patch
+  nixpkgs.overlays = [
+    # GNOME 46: triple-buffering-v4-46
+    (final: prev: {
+      gnome = prev.gnome.overrideScope (gnomeFinal: gnomePrev: {
+        mutter = gnomePrev.mutter.overrideAttrs ( old: {
+          src = pkgs.fetchgit {
+            url = "https://gitlab.gnome.org/vanvugt/mutter.git";
+			rev = "663f19bc02c1b4e3d1a67b4ad72d644f9b9d6970";
+            sha256 = "sha256-I1s4yz5JEWJY65g+dgprchwZuPGP9djgYXrMMxDQGrs=";         
+          };
+        } );
+      });
+    })
+  ];
 
 }
