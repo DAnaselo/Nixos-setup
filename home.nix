f306d7b6f9f5d7b4d7dfa146b37c2bcd518b606d
DAnaselo <abukaffanas5@gmail.com>
2024-05-28 22:35:17 +0300

    i'm sexually confused

--- home.nix
+++ home.nix
@@ -85,12 +85,10 @@
    };
  };
 
- home.file.".config/hypr/hyprland.conf".source = ./dots/hypr/hyprland.conf;
- home.file.".config/hypr/hyprpaper.conf".source = ./dots/hypr/hyprpaper.conf;
- home.file.".config/waybar/config".source = ./dots/waybar/config;
- home.file.".config/dunst/dunstrc".source = ./dots/dunst/dunstrc;
- home.file.".config/waybar/style.css".source = ./dots/waybar/style.css;
- home.file.".config/rofi/config.rasi".source = ./dots/rofi/config.rasi;
+ home.file.".config/hypr".source = ./dots/hypr;
+ home.file.".config/waybar".source = ./dots/waybar;
+ home.file.".config/dunst".source = ./dots/dunst;
+ home.file.".config/rofi".source = ./dots/rofi;
 
   # Homemanager Version
   home.stateVersion = "24.11";
